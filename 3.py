a63f2fe5fc71268d6d2be1b69e500e0dd657196d
Fabiannossa <131404605+Fabiannossa@users.noreply.github.com>
2023-04-20 18:24:28 -0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,32 @@
+# Calcular la circunferencia de un círculo dado su radio
+
+import math
+
+radio = 3
+circunferencia = 2 * math.pi * radio
+# Redondea el valor de la circunferencia a 6 decimales
+circunferencia_redondeada = round(circunferencia, 6)
+
+print("La circunferencia de un círculo con radio", radio, "es:", circunferencia_redondeada)
+
+# Calcular la circunferencia de un círculo dado su radio
+
+import math
+
+radio = 8
+circunferencia = 2 * math.pi * radio
+# Redondea el valor de la circunferencia a 6 decimales
+circunferencia_redondeada = round(circunferencia, 6)
+
+print("La circunferencia de un círculo con radio", radio, "es:", circunferencia_redondeada)
+
+# Calcular la circunferencia de un círculo dado su radio
+
+import math
+
+radio = 10
+circunferencia = 2 * math.pi * radio
+# Redondea el valor de la circunferencia a 6 decimales
+circunferencia_redondeada = round(circunferencia, 6)
+
+print("La circunferencia de un círculo con radio", radio, "es:", circunferencia_redondeada)
